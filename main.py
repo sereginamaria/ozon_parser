4eb8152ea287f2d75b0081da6bf27a943af55c38
Maria <60785083+sereginamaria@users.noreply.github.com>
2023-06-19 08:00:22 +0300

    get json

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import undetected_chromedriver
 import time
 import json
 
+from selenium.webdriver.common.by import By
+
 try:
     driver = undetected_chromedriver.Chrome()
     #driver.get("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")
-    response = driver.get("https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url=/product/plate-tvoe-560721955/?asb=V4JBqdAITO1%252FxsJPEHJidk%252BRpUjJEhJUJ82VKaUTFq4%253D&asb2=eaWmaXTL-onw4EOIyuFdpQHq95AELbcEFAJjPe6qnMX4qTOh8G7H0vfeMjVceQ-n&avtc=1&avte=2&avts=1686903024&keywords=%D0%9B%D0%B5%D1%82%D0%BD%D0%B5%D0%B5+%D0%BF%D0%BB%D0%B0%D1%82%D1%8C%D0%B5&layout_container=pdpApparelPage2&layout_page_index=2&sh=chmEfcpKoQ")
-    # time.sleep(15)
+    response = driver.get("https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url=/product/plate-tvoe-560721955/")
+    # time.sleep(1)
 
-    print('djsvkn')
+    # print('djsvkn')
+    # print(type(driver.page_source))
+
+    from pprint import pprint
+    # pprint(driver.page_source)
+
+    content = driver.page_source.replace(u'\u2009', ',')
+    content = driver.find_element(By.TAG_NAME, 'pre').text.replace(u'\u2009', ' ')
+    parsed_json = json.loads(content)
+    # print(type(parsed_json))
+    # print(parsed_json)
+    #pre = driver.find_element_by_tag_name("pre").text
+
+    with open('new_file.json', 'w', encoding="utf-8") as outfile:
+        json.dump(parsed_json, outfile, indent=4, sort_keys=False, ensure_ascii=False,separators=(',', ': '))
     # print(type(driver.page_source))
     # print(driver.page_source.json())
-
-    newjson = json.loads(driver.page_source)
-    print(newjson)
-
-
 
 except Exception as ex:
     print(ex)
