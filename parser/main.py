6f063f512cff63de65aa4cbe018ca5b3767c7a69
sereginamaria <v44882266>
2023-08-03 16:04:20 +0300

    progress

--- parser/main.py
+++ parser/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from parser.get_product import get_product
 from parser.get_products_from_page import get_products_from_page
-
+from flask import request
 app = Flask(__name__)
 
 
@@ -17,22 +17,21 @@ def get_page():
     return 'Получаем ссылки на продукты со страниц'
 
 
-@app.post('/get_product')
-def get_ozon_product(message):
-    print('123')
-    print(message)
-    get_product(message)
-    return 'Получаем информацию о товаре'
-
+@app.route('/get_product', methods=['GET', 'POST'])
+def get_ozon_product():
+    # print('123')
+    # print(message)
+    # get_product(message)
+    # return 'Получаем информацию о товаре'
     #
-    # if request.method == 'POST':
-    #     print('123')
-    #     get_product("/product/plate-1040289631/")
-    #     return 'Получаем информацию о товаре'
-    # if request.method == 'POST':
-    #     print('123')
-    #     get_product("/product/plate-1040289631/")
-    #     return 'Получаем информацию о товаре'
+
+    if request.method == 'POST':
+        print('123')
+        print(request)
+        return 'Получаем информацию о товаре'
+    if request.method == 'GET':
+        print('GET')
+        return 'Получаем информацию о товаре'
 
 
 if __name__ == "__main__":
