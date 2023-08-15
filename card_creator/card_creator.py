edc49636dcefd3729479ce74418275bddaee8030
Maria <60785083+sereginamaria@users.noreply.github.com>
2023-08-15 12:30:05 +0300

    fixed card rendering

--- card_creator/card_creator.py
+++ card_creator/card_creator.py
@@ -4,18 +4,20 @@ from html2image import Html2Image
 
 telegram_url = "https://api.telegram.org/bot6508472057:AAHdRDqUbaVjn7sstEtnHPMmKAXXAPp6_og"
 
+
 def card_creator(message):
     print(message)
 
-    # render_html = render()
+    render_html = render()
     # f = open('card_creator/render_html.html', 'w', encoding='UTF-8')  # открытие в режиме записи
     # f.write(render_html)
 
-    card()
+    card(render_html)
 
-def card():
+
+def card(html):
     hti = Html2Image(
-        output_path='card_creator',
+        output_path='card_creator/',
         custom_flags=[
             '--no-sandbox'
             '--remote-allow-origins=*',
@@ -24,7 +26,7 @@ def card():
     )
 
     hti.screenshot(
-        html_file='card_creator/render_html.html',
+        html_str=html,
         save_as='card.png', size=(1024, 1280)
     )
 
@@ -34,5 +36,13 @@ def card():
         data={'chat_id': 6181726421}, files=files
     )
 
+
 def render():
     return render_template('cardtest.html', name='eee')
+
+
+if __name__ == '__main__':
+    from parser.main import app
+
+    with app.app_context():
+        card_creator("dsfsdf")
