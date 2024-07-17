d3243b03e2bcfba11743bfb82515c099212752c9
sereginamaria <v44882266>
2024-07-17 13:41:10 +0300

    Создание компонента AdminPanel.vue

---
+++ frontend/src/components/AdminPanel.vue
@@ -0,0 +1,23 @@
+
+<template>
+<div>
+      <h3>Работа с данными</h3>
+      <div>
+        <Button  label="Отложить текущую категорию товаров" style="margin-right: 5%"/>
+        <Button  label="Вернуть все отложенные товары"/>
+      </div>
+      <Button  label="Показать расписание" style="margin-top: 1%"/>
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AdminPanel"
+})
+</script>
+
+<style scoped>
+
+</style>
