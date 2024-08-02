07cd1a26915674961262b8f192b50ef29bd8ea19
sereginamaria <v44882266>
2024-08-02 14:00:52 +0300

    Update dag

--- backend/airflow/send_trousers.py
+++ backend/airflow/send_trousers.py
@@ -5,76 +5,44 @@ from airflow.operators.python import PythonOperator
 from datetime import timedelta
 from typing import Optional
 from pendulum import Date, DateTime, Time, timezone
-
 from airflow.plugins_manager import AirflowPlugin
 from airflow.timetables.base import DagRunInfo, DataInterval, TimeRestriction, Timetable
+from airflow.timetables.events import EventsTimetable
+from multiple_crons_timetable import MultiCronTimetable
 
 
-UTC = timezone("UTC")
-
-# class UnevenIntervalsTimetable(Timetable):
-#
-#     def infer_manual_data_interval(self, run_after: DateTime) -> DataInterval:
-#         delta = timedelta(days=1)
-#         # If time is between 6:00 and 16:30, period ends at 6am and starts at 16:30 previous day
-#         if run_after >= run_after.set(hour=6, minute=0) and run_after <= run_after.set(hour=16, minute=30):
-#             start = (run_after-delta).set(hour=16, minute=30, second=0).replace(tzinfo=UTC)
-#             end = run_after.set(hour=6, minute=0, second=0).replace(tzinfo=UTC)
-#         # If time is after 16:30 but before midnight, period is between 6:00 and 16:30 the same day
-#         elif run_after >= run_after.set(hour=16, minute=30) and run_after.hour <= 23:
-#             start = run_after.set(hour=6, minute=0, second=0).replace(tzinfo=UTC)
-#             end = run_after.set(hour=16, minute=30, second=0).replace(tzinfo=UTC)
-#         # If time is after midnight but before 6:00, period is between 6:00 and 16:30 the previous day
-#         else:
-#             start = (run_after-delta).set(hour=6, minute=0).replace(tzinfo=UTC)
-#             end = (run_after-delta).set(hour=16, minute=30).replace(tzinfo=UTC)
-#         return DataInterval(start=start, end=end)
-#
-#     def next_dagrun_info(
-#         self,
-#         *,
-#         last_automated_data_interval: Optional[DataInterval],
-#         restriction: TimeRestriction,
-#     ) -> Optional[DagRunInfo]:
-#         if last_automated_data_interval is not None:  # There was a previous run on the regular schedule.
-#             last_start = last_automated_data_interval.start
-#             delta = timedelta(days=1)
-#             if last_start.hour == 6: # If previous period started at 6:00, next period will start at 16:30 and end at 6:00 following day
-#                 next_start = last_start.set(hour=16, minute=30).replace(tzinfo=UTC)
-#                 next_end = (last_start+delta).replace(tzinfo=UTC)
-#             else: # If previous period started at 14:30, next period will start at 6:00 next day and end at 14:30
-#                 next_start = (last_start+delta).set(hour=6, minute=0).replace(tzinfo=UTC)
-#                 next_end = (last_start+delta).replace(tzinfo=UTC)
-#         else:  # This is the first ever run on the regular schedule. First data interval will always start at 6:00 and end at 16:30
-#             next_start = restriction.earliest
-#             if next_start is None:  # No start_date. Don't schedule.
-#                 return None
-#             if not restriction.catchup: # If the DAG has catchup=False, today is the earliest to consider.
-#                 next_start = max(next_start, DateTime.combine(Date.today(), Time.min).replace(tzinfo=UTC))
-#             next_start = next_start.set(hour=6, minute=0).replace(tzinfo=UTC)
-#             next_end = next_start.set(hour=16, minute=30).replace(tzinfo=UTC)
-#         if restriction.latest is not None and next_start > restriction.latest:
-#             return None  # Over the DAG's scheduled end; don't schedule.
-#         return DagRunInfo.interval(start=next_start, end=next_end)
-#
-# class UnevenIntervalsTimetablePlugin(AirflowPlugin):
-#     name = "uneven_intervals_timetable_plugin"
-#     timetables = [UnevenIntervalsTimetable]
+def send_trousers(**kwargs):
+    print('1')
+    print('Hello from {kw}'.format(kw=kwargs['my_keyword']))
+#     тут надо отправить запрос на /send_post и передать категорию и субкатегорию
 
 
 send_trousers_dag = DAG(
     dag_id="send_trousers_dag",
     start_date=datetime.datetime(2024, 8, 1),
-    schedule_interval='0 12 * * 1, 0 16 * * 3',
+    timetable=MultiCronTimetable(['0 12 * * mon', '0 16 * * wed', '0 12 * * fri', '0 18 * * sat', '*/5 * * * *'], timezone='Europe/Moscow', period_length=10, period_unit='minutes'),
 )
-
-
-def send_trousers():
-    print('1')
-#     тут надо отправить запрос на /send_post и передать категорию и субкатегорию
 
 PythonOperator(
     task_id="parse_trousers",
     python_callable=send_trousers,
     dag=send_trousers_dag,
+    op_kwargs={'my_keyword': 'Airflow'}
+
+
+)
+
+send_trousers_dag = DAG(
+    dag_id="send_trousers_dag",
+    start_date=datetime.datetime(2024, 8, 1),
+    timetable=MultiCronTimetable(['0 12 * * mon', '0 16 * * wed', '0 12 * * fri', '0 18 * * sat', '*/5 * * * *'], timezone='Europe/Moscow', period_length=10, period_unit='minutes'),
+)
+
+PythonOperator(
+    task_id="parse_trousers",
+    python_callable=send_trousers,
+    dag=send_trousers_dag,
+    op_kwargs={'my_keyword': 'Airflow'}
+
+
 )
